67cb61162054fc4a0f76eee3b105693027a0696c
Shauna Griffin <shaunagriffin@Shaunas-MacBook-Pro.local>
2020-12-01 21:02:27 +0100

    1st commit

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+import './App.css';
 import React from "react";
 import { Switch, Route } from "react-router-dom";
 
@@ -9,14 +10,16 @@ import "./App.css";
 import React from "react";
 import { useEffect, useState } from "react";
 import { makeStyles } from "@material-ui/core/styles";
-import Paper from "@material-ui/core/Paper";
 import Grid from "@material-ui/core/Grid";
 import TextField from "@material-ui/core/TextField";
 import CssBaseline from "@material-ui/core/CssBaseline";
 import Container from "@material-ui/core/Container";
 import food from "./food.jpg";
+import CssBaseline from '@material-ui/core/CssBaseline';
+import Container from '@material-ui/core/Container';
 import Restaurant from "./Restaurant.js";
 import SearchBanner from "./SearchBanner.js";
+import Carousel from 'react-material-ui-carousel'
 
 const useStyles = makeStyles((theme) => ({
   root: {
@@ -51,6 +54,24 @@ function App() {
         <Route exact path="/header" component={Header} />
         <Route path="/singlerestaurant/:id" component={SingleRestaurant} />
       </Switch>
+    <React.Fragment>
+      <CssBaseline />
+      <Container z-index="10" maxWidth="xl">
+        <div className={classes.root}>
+          <Grid container spacing={3}>
+
+          <SearchBanner/>
+          <Grid item xl={9} xs={9} sm={9}>
+            <Carousel indicators={false} navButtonsAlwaysVisible={true} >
+            {
+              data.restaurants.map((rest, i) => <Restaurant  key={i} picture={rest.photos[0].links[0]} name={rest.name} type={rest.cuisine} address={rest.formatted_address} />)
+            }
+            </Carousel>
+          </Grid>
+          </Grid>
+        </div>
+      </Container>
+    </React.Fragment>
 
       <React.Fragment>
         <CssBaseline />
