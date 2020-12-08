1a009e8da423a0fea8c34914a27650752a421c28
annamariaratajczak <annaratajczak89@gmail.com>
2020-12-08 19:16:19 +0100

    search

--- src/components/Search/index.js
+++ src/components/Search/index.js
@@ -1,6 +1,7 @@
 import React from "react";
 import TextField from '@material-ui/core/TextField';
 import { makeStyles } from "@material-ui/core/styles";
+import InputBase from '@material-ui/core/InputBase';
 
 
 const useStyles = makeStyles((theme) => ({
@@ -8,15 +9,23 @@ const useStyles = makeStyles((theme) => ({
      // backgroundColor: "white",
       background: "rgba(255, 255, 255, 0.7)",
       width: "300px",
-      borderRadius: 30
+      borderRadius: 30,
+      "&:focus": {
+        border: "none",
+        cssOutlinedInput: {
+          '&$cssFocused $notchedOutline': {
+            border: `none`,
+          }
+        },
   }
+}
 }));
 
 function Search({ handleChange }) {
   const classes = useStyles();
   return (
   
-      <TextField className={classes.searchField} onChange={handleChange} id="outlined-search" label="Find restaurant" type="search" variant="outlined" />
+      <InputBase className={classes.searchField} onChange={handleChange} id="outlined-search" placeholder="Find restaurant" type="search" variant="outlined" />
 
   );
 }
