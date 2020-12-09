f1197f2dada9118a0fb49ea3a2a69f8b8dc3cc9c
Gregfanyan <grigorfanyan@gmail.com>
2020-12-09 12:21:01 +0100

    refactor

--- src/App.js
+++ src/App.js
@@ -1,4 +1,3 @@
-import "./App.css";
 import React from "react";
 
 import { Switch, Route } from "react-router-dom";
@@ -6,16 +5,13 @@ import Home from "./components/Home";
 import Header from "./components/Header";
 import SingleRestaurant from "./components/SingleRestaurant";
 
-
-
+import "./App.css";
 
 function App() {
-
   return (
     <div>
       <Switch>
         <Route exact path="/" component={Home} />
-        <Route exact path="/header" component={Header} />
         <Route path="/singlerestaurant/:id" component={SingleRestaurant} />
       </Switch>
     </div>
